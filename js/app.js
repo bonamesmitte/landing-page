88cc55ab9e16293cd94beb47fb8206a8cfc9e7e2
Alice <innycity84@gmail.com>
2020-12-09 03:53:44 -0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,57 +1,12 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+let music = document.getElementById("music");
+let IconPlay = document.getElementsByClass("far fa-play-circle")
+let IconPause = document.getElementsByClassName("far fa-pause-circle")
 
-/**
- * Define Global Variables
- * 
-*/
+IconPlay.onclick = function(){
+    music.play();
+}
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+IconPause.onclick = function(){
+    music.pause();
+}
 
